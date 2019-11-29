a4c37e9fd96fb26b71bd3b594a2c021a20fbd8e3
viviRbi <50475727+viviRbi@users.noreply.github.com>
2019-11-28 20:21:53 -0600

    OOP practice

---
+++ script.js
@@ -0,0 +1,33 @@
+// ------------------------------------------------DICE GAME ---------------------------------
+
+// const game = new DiceGame();
+
+// const dice = document.createElement('div');
+// dice.textContent = "roll here";
+// document.body.appendChild(dice);
+ 
+// dice.addEventListener('click',function(){
+// 	let playerRoll = game.roll();
+// 	let compRoll = game.roll();
+// 	let winner = game.checker(playerRoll, compRoll); 
+// 	console.log(winner);
+// 	dice.innerHTML =`Player ${playerRoll} vs ${compRoll} <br> ${winner}`;
+// })
+
+// function DiceGame(){
+// 	this.roll = function(){
+// 		this.result = Math.floor(Math.random()*6 )+1;
+// 		return this.result;
+// 		}
+// 		this. checker = function (roll1, roll2){
+// 			if(roll1>roll2){
+// 				return 'Player Wins'
+// 			} else if (roll1>roll2){
+// 				return 'Computer Wins'
+// 			} else {
+// 				return "Tie"
+// 			}
+// 		}
+// 	}
+
+// ------------------------------------------------ Shopping Cart ---------------------------------
