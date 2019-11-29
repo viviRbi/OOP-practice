49efa01fb1b1f7206f37e9342317f114d13f11fa
viviRbi <50475727+viviRbi@users.noreply.github.com>
2019-11-28 20:42:19 -0600

    Update

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 // ------------------------------------------------DICE GAME ---------------------------------
 
-// const game = new DiceGame();
+const game = new DiceGame();
 
-// const dice = document.createElement('div');
-// dice.textContent = "roll here";
-// document.body.appendChild(dice);
+const dice = document.createElement('div');
+dice.textContent = "roll here";
+document.body.appendChild(dice);
  
-// dice.addEventListener('click',function(){
-// 	let playerRoll = game.roll();
-// 	let compRoll = game.roll();
-// 	let winner = game.checker(playerRoll, compRoll); 
-// 	console.log(winner);
-// 	dice.innerHTML =`Player ${playerRoll} vs ${compRoll} <br> ${winner}`;
-// })
+dice.addEventListener('click',function(){
+	let playerRoll = game.roll();
+	let compRoll = game.roll();
+	let winner = game.checker(playerRoll, compRoll); 
+	console.log(winner);
+	dice.innerHTML =`Player ${playerRoll} vs ${compRoll} <br> ${winner}`;
+})
 
-// function DiceGame(){
-// 	this.roll = function(){
-// 		this.result = Math.floor(Math.random()*6 )+1;
-// 		return this.result;
-// 		}
-// 		this. checker = function (roll1, roll2){
-// 			if(roll1>roll2){
-// 				return 'Player Wins'
-// 			} else if (roll1>roll2){
-// 				return 'Computer Wins'
-// 			} else {
-// 				return "Tie"
-// 			}
-// 		}
-// 	}
+function DiceGame(){
+	this.roll = function(){
+		this.result = Math.floor(Math.random()*6 )+1;
+		return this.result;
+		}
+	this. checker = function (roll1, roll2){
+		if(roll1>roll2){
+			return 'Player Wins'
+		} else if (roll1>roll2){
+			return 'Computer Wins'
+		} else {
+			return "Tie"
+		}
+	}
+}
 
 // ------------------------------------------------ Shopping Cart ---------------------------------
+
+const output = document.createElement('div');
+document.body.appendChild(output)
+
+constoutput1 = document.createElement('div')
